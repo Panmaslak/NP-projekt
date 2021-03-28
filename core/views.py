91883104f32ca630aa0bea548ced6d6bde1779f4
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-03-28 22:13:18 +0200

    Konfiguracja projektu

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.template import context
+from .models import Item
 
-# Create your views here.
+
+def products(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "products.html", context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def home(request):
+    return render(request, "home.html")
