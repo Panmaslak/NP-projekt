8d9791624a563ca6d86f9801b0853c0ac80ad9dc
JakubKujawa <Kujawa_Jakub@yahoo.com>
2021-04-19 20:48:38 +0200

    Wiele usprawnień (opis) > Lepsza jakość kodu > Kliknięcie na tytuł artykułu przechodzi do szczegołów > Zlikwidowanie wielu placeholderów

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (
-    products,
-    checkout,
-    home
+    HomeView,
+    ItemDetailView,
+    checkout
 )
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
-    path('products/', products, name='products')
+    path('product/<slug>/', ItemDetailView.as_view(), name='product')
 ]
